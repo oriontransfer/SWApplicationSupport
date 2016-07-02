304b36780eef40b70003278bb7127861f9bc222f
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2016-07-02 16:25:16 +1200

    Support for Swift.

---
+++ SWApplicationSupport.h
@@ -0,0 +1,19 @@
+//
+//  SWApplicationSupport.h
+//  SWApplicationSupport
+//
+//  Created by Samuel Williams on 14/02/16.
+//  Copyright © 2016 Orion Transfer Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SWApplicationSupport.
+FOUNDATION_EXPORT double SWApplicationSupportVersionNumber;
+
+//! Project version string for SWApplicationSupport.
+FOUNDATION_EXPORT const unsigned char SWApplicationSupportVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWApplicationSupport/PublicHeader.h>
+
+
